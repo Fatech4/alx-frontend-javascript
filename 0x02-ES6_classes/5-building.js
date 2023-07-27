1393bf558e58f2bb6c8ecff68a879802e8124582
Faruk <adekoladamilola4@gmail.com>
2023-07-27 23:07:19 +0100

    Done with task 5

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,17 @@
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+  }
+
+  // Getters
+  get sqft() {
+    return this._sqft;
+  }
+
+  // Abstract method
+  evacuationWarningMessage() {
+    throw new Error(
+      'Class extending Building must override evacuationWarningMessage'
+    );
+  }
+}
