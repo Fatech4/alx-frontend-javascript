45ab2dd0abf60e8f551461e63ce91bd184fb1547
Faruk <adekoladamilola4@gmail.com>
2023-07-28 22:05:57 +0100

    Done with task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,3 +1,11 @@
-export default handleResponseFromAPI(promise) {
-
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return {
+        status: 200,
+        body: 'success',
+      };
+    })
+    .catch(() => new Error()); // Returning an empty Error object for every rejection
 }
