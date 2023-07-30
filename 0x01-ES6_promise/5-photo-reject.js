99f4bd63258c8c3f83618acf04bf6957ad27ace6
Faruk <adekoladamilola4@gmail.com>
2023-07-30 18:14:22 +0100

    fixes

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,5 +1,11 @@
 export default function uploadPhoto(fileName) {
-  return new Promise((reject) => {
-    reject(new Error(`${fileName} cannot be processed`));
+  return new Promise((resolve, reject) => {
+    const processingFailed = true;
+    if (processingFailed) {
+      reject(new Error(`${fileName} cannot be processed`));
+    } else {
+      // Simulate successful processing
+      resolve(`${fileName} has been successfully processed`);
+    }
   });
 }
