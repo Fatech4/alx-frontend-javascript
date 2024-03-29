21d56a95939bb1fedd9491d7548ab41e205103ab
Faruk <adekoladamilola4@gmail.com>
2024-03-29 12:08:27 +0100

    Task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,12 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then((values) => {
-      console.log(
-        `${values[0].body} ${values[1].firstName} ${values[1].lastName}`
-      );
-    })
-    .catch(() => {
-      console.error('Signup system offline');
-    });
+async function handleProfileSignup() {
+  try {
+    const [photoResponse, userResponse] = await Promise.all([uploadPhoto(), createUser()]);
+    console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+  } catch (error) {
+    console.error('Signup system offline');
+  }
 }
+
+handleProfileSignup();
