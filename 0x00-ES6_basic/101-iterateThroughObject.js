bf092061c7e74e97c74d6a1ae3a753967d479a05
Faruk <adekoladamilola4@gmail.com>
2024-03-29 11:48:37 +0100

    fix

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -6,8 +6,10 @@ export default function iterateThroughObject(reportWithIterator) {
     result += `${employee} | `;
   }
 
-  // Remove the last ' | ' from the result
-  result = result.slice(0, -3);
+  // Remove the trailing ' | ' from the result string
+  if (result.length > 0) {
+    result = result.slice(0, -3); // Remove the last 3 characters
+  }
 
   return result;
 }
